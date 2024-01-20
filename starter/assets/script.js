e58bb724a76ea70ab57e80f7a8dc890cf7aaa95b
Abdalla Diaai <56742529+abdalla-diaai@users.noreply.github.com>
2024-01-20 02:54:56 +0000

    decreased padding of header and added margin bottom to container.

--- starter/assets/script.js
+++ starter/assets/script.js
@@ -9,19 +9,16 @@ var eventToSave;
 var eventID;
 var storedEvents;
 
-
-
-
 function saveEvent(event) {
     // to traverse DOM from parent time block to children textarea
     if (!dayEvents) {
         dayEvents = {};
     };
+    // get stored events if existent
     storedEvents = JSON.parse(localStorage.getItem('event'));
     if (storedEvents !== null) {
         storedEvents = dayEvents;
-    }
-
+    };
     eventToSave = $(event.target).parent().children('textarea');
     eventID = eventToSave.attr('id');
     if (eventID in dayEvents) {
@@ -29,11 +26,10 @@ function saveEvent(event) {
          
     };
     dayEvents[eventID] = eventToSave.val();
-    
     localStorage.setItem('event', JSON.stringify(dayEvents));
 };
 
-function getSavedEvents() {
+function renderSavedEvents() {
     eventsArray = JSON.parse(localStorage.getItem('event'));
     if (eventsArray) {
         for (const [key, value] of Object.entries(eventsArray)) {
@@ -42,13 +38,12 @@ function getSavedEvents() {
     };
 };
 
-
-
 // each function iterate through each element and compare current time to time block time and color accordingly past, present and future
 
 plannerHours.each(function () {
     var value = $(this).text();
     var hoursOnly = value.split(':')[0];
+    // get hours and convert to 24 hours
     if (value.includes('PM') && hoursOnly < 12) {
         hoursOnly = Number(hoursOnly) + 12;
     };
@@ -63,10 +58,12 @@ plannerHours.each(function () {
 });
 
 $('#currentDay').text(todayDate);
+
 // set interval to update date every 24 hours
 setInterval(() => {
     $('#currentDay').text(today);
 }, (1000 * 60 * 60 * 24));
 
+// run main function to save events on click
 timeBlocks.on('click', '.saveBtn', saveEvent);
-getSavedEvents()
+renderSavedEvents();
