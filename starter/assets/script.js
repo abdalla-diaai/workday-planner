4023802ca6bea185d6452d60adfd3ca49cd58aab
Abdalla Diaai <56742529+abdalla-diaai@users.noreply.github.com>
2024-01-20 02:38:20 +0000

    fixed function to store and render local storage.

--- starter/assets/script.js
+++ starter/assets/script.js
@@ -4,7 +4,63 @@ var currentTime = dayjs().format('HH');
 var saveButton = $('.saveBtn');
 var timeBlocks = $('.time-block');
 var plannerHours = $('.hour');
+var dayEvents;
 var eventToSave;
+var eventID;
+var storedEvents;
+
+
+
+
+function saveEvent(event) {
+    // to traverse DOM from parent time block to children textarea
+    if (!dayEvents) {
+        dayEvents = {};
+    };
+    storedEvents = JSON.parse(localStorage.getItem('event'));
+    if (storedEvents !== null) {
+        storedEvents = dayEvents;
+    }
+
+    eventToSave = $(event.target).parent().children('textarea');
+    eventID = eventToSave.attr('id');
+    if (eventID in dayEvents) {
+        localStorage.removeItem(eventID);
+         
+    };
+    dayEvents[eventID] = eventToSave.val();
+    
+    localStorage.setItem('event', JSON.stringify(dayEvents));
+};
+
+function getSavedEvents() {
+    eventsArray = JSON.parse(localStorage.getItem('event'));
+    if (eventsArray) {
+        for (const [key, value] of Object.entries(eventsArray)) {
+            $(`#${key}`).text(value);
+        };
+    };
+};
+
+
+
+// each function iterate through each element and compare current time to time block time and color accordingly past, present and future
+
+plannerHours.each(function () {
+    var value = $(this).text();
+    var hoursOnly = value.split(':')[0];
+    if (value.includes('PM') && hoursOnly < 12) {
+        hoursOnly = Number(hoursOnly) + 12;
+    };
+    if (hoursOnly > currentTime) {
+        var element = $(`#${hoursOnly}`)
+        element.addClass('future')
+    } else if (hoursOnly == currentTime) {
+        $(`#${hoursOnly}`).addClass('present')
+    } else {
+        $(`#${hoursOnly}`).addClass('past')
+    };
+});
 
 $('#currentDay').text(todayDate);
 // set interval to update date every 24 hours
@@ -12,29 +68,5 @@ setInterval(() => {
     $('#currentDay').text(today);
 }, (1000 * 60 * 60 * 24));
 
-
-function saveEvent(event) {
-    var buttonClicked = event.target;
-    console.log(buttonClicked.text())
-};
-
 timeBlocks.on('click', '.saveBtn', saveEvent);
-
-// each function iterate through each element and compare current time to time block time and color accordingly past, present and future
-
-plannerHours.each(function() {
-    var value = $(this).text();
-    var hoursOnly = value.split(':')[0];
-    console.log(hoursOnly);
-        if (value.includes('PM') && hoursOnly < 12) {
-            hoursOnly = Number(hoursOnly) + 12;
-        };
-        if (hoursOnly > currentTime) {
-            var element = $(`#${hoursOnly}`)
-            element.addClass('future')
-        } else if (hoursOnly == currentTime) {
-            $(`#${hoursOnly}`).addClass('present')
-        } else {
-            $(`#${hoursOnly}`).addClass('past')
-        };
-});
+getSavedEvents()
