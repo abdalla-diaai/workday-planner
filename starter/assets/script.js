19ce25e69f9769eed8516fe64a1b4f0120aaec05
Abdalla Diaai <56742529+abdalla-diaai@users.noreply.github.com>
2024-01-20 00:00:47 +0000

    added colors to time blocks based on current time.

--- starter/assets/script.js
+++ starter/assets/script.js
@@ -1,8 +1,40 @@
 // variable to store date 
-var today = dayjs().format('D dddd, MMMM, YYYY');
-$('#currentDay').text(today);
+var todayDate = dayjs().format('D dddd, MMMM, YYYY');
+var currentTime = dayjs().format('HH');
+var saveButton = $('.saveBtn');
+var timeBlocks = $('.time-block');
+var plannerHours = $('.hour');
+var eventToSave;
+
+$('#currentDay').text(todayDate);
 // set interval to update date every 24 hours
 setInterval(() => {
     $('#currentDay').text(today);
 }, (1000 * 60 * 60 * 24));
 
+
+function saveEvent(event) {
+    var buttonClicked = event.target;
+    console.log(buttonClicked.text())
+};
+
+timeBlocks.on('click', '.saveBtn', saveEvent);
+
+// each function iterate through each element and compare current time to time block time and color accordingly past, present and future
+
+plannerHours.each(function() {
+    var value = $(this).text();
+    var hoursOnly = value.split(':')[0];
+    console.log(hoursOnly);
+        if (value.includes('PM') && hoursOnly < 12) {
+            hoursOnly = Number(hoursOnly) + 12;
+        };
+        if (hoursOnly > currentTime) {
+            var element = $(`#${hoursOnly}`)
+            element.addClass('future')
+        } else if (hoursOnly == currentTime) {
+            $(`#${hoursOnly}`).addClass('present')
+        } else {
+            $(`#${hoursOnly}`).addClass('past')
+        };
+});
